9a437d792f67d41231a7c8b6fce10ae38be0db55
md Yousuf <76865383+Yousuf408@users.noreply.github.com>
2026-06-07 20:12:34 +0530

    Update scanner.py

--- pages/scanner.py
+++ pages/scanner.py
@@ -836,8 +836,8 @@ def run_full_scan(watchlist_stocks: list):
 
     import concurrent.futures
 
-    BATCH_SIZE  = 3
-    BATCH_WAIT  = 1.5
+    BATCH_SIZE  = 30
+    BATCH_WAIT  = 1
 
     st.session_state.is_scanning  = True
     st.session_state.scan_log     = []
