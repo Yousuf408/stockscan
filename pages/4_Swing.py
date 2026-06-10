fd2e7a526198e9fe1c31a7d10a24ea3e70206a3e
md Yousuf <76865383+Yousuf408@users.noreply.github.com>
2026-06-10 23:45:33 +0530

    Update 4_Swing.py

--- pages/4_Swing.py
+++ pages/4_Swing.py
@@ -645,7 +645,7 @@ for r in view:
 
     st.markdown(
         f"<div style='border-left:3px solid {bc};margin-bottom:0;"
-        f"border-bottom:1px solid #ffffff;'></div>",
+        f"border-bottom:1px solid #FFFFFF;'></div>",
         unsafe_allow_html=True,
     )
 
