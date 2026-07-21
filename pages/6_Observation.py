ecc9854512b805c11f77f678c3dec188b9cc2cc3
md Yousuf <76865383+Yousuf408@users.noreply.github.com>
2026-07-21 23:23:42 +0530

    Update 6_Observation.py

--- pages/6_Observation.py
+++ pages/6_Observation.py
@@ -554,7 +554,7 @@ def get_tradingview_stocks():
                 col('exchange') == 'NSE'
             )
             .order_by('change', ascending=False)
-            .limit(1000)
+            .limit(100)
             .get_scanner_data()
         )
         return count, df
